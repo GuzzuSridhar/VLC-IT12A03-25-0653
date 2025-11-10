5e312b5204f42917d605d8c9466f352603acca83
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2025-11-10 15:03:39 +0530

    add special numbers

---
+++ src/basics/SpecialNumbers.java
@@ -0,0 +1,16 @@
+package basics;
+
+public class SpecialNumbers {
+    public static void main(String[] args) {
+        // Octal numbers prefixed with zero
+        int oct = 077;
+        // Binary numbers prefixed with "0b"
+        int bin = 0b001;
+        // Hexadecimal numbers prefixed with "0x"
+        int hex = 0x01ABF;
+
+        System.out.println(oct);
+        System.out.println(bin);
+        System.out.println(hex);
+    }
+}
