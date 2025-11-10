0a406e54b671f79a29c22bdb5e9ab414d9fee7a4
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2025-11-10 14:36:36 +0530

    add max and Min

---
+++ src/basics/WrapperClasses.java
@@ -0,0 +1,46 @@
+package basics;
+
+public class WrapperClasses {
+    public static void main(String[] args) {
+        // Wrapper classes
+        // every primitive type has a wrapper class associated
+        // used to get / access meta data (methods) of the object
+        /*
+         * Primitive types --> Wrapper class
+         * byte -- Byte
+         * short -- Short
+         * int -- Integer
+         * long -- Long
+         * float -- Float
+         * double -- Double
+         */
+
+        // Meta data of primitive types using the wrapper classes
+        System.out.println("---------------------Byte Type------------------");
+        System.out.println("The size of Byte type is: " + Byte.SIZE);
+        System.out.println("The Mininum value of Byte type is: " + Byte.MIN_VALUE);
+        System.out.println("The Maximum Value of Byte type is: " + Byte.MAX_VALUE);
+        System.out.println("---------------------Short Type------------------");
+        System.out.println("The size of Short type is: " + Short.SIZE);
+        System.out.println("The Mininum value of Short type is: " + Short.MIN_VALUE);
+        System.out.println("The Maximum Value of Short type is: " + Short.MAX_VALUE);
+        System.out.println("---------------------Integer Type------------------");
+        System.out.println("The size of Integer type is: " + Integer.SIZE);
+        System.out.println("The Mininum value of Integer type is: " + Integer.MIN_VALUE);
+        System.out.println("The Maximum Value of Integer type is: " + Integer.MAX_VALUE);
+        System.out.println("---------------------Long Type------------------");
+        System.out.println("The size of Long type is: " + Long.SIZE);
+        System.out.println("The Mininum value of Long type is: " + Long.MIN_VALUE);
+        System.out.println("The Maximum Value of Long type is: " + Long.MAX_VALUE);
+        System.out.println("---------------------Float Type------------------");
+        System.out.println("The size of Float type is: " + Float.SIZE);
+        System.out.println("The Mininum value of Float type is: " + Float.MIN_VALUE);
+        System.out.println("The Maximum Value of Float type is: " + Float.MAX_VALUE);
+        System.out.println("---------------------Double Type------------------");
+        System.out.println("The size of Double type is: " + Double.SIZE);
+        System.out.println("The Mininum value of Double type is: " + Double.MIN_VALUE);
+        System.out.println("The Maximum Value of Double type is: " + Double.MAX_VALUE);
+
+    }
+
+}
