03d07b2d6a275abfaafe4278776e56463b0040a2
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2025-11-10 13:23:56 +0530

    add datatypes

---
+++ src/basics/DataTypes.java
@@ -0,0 +1,62 @@
+package basics;
+
+public class DataTypes {
+    public static void main(String[] args) {
+        /*
+         * Primitive types
+         * byte
+         * short
+         * int
+         * long
+         * float
+         * double
+         * char
+         * boolean
+         */
+        // object
+
+        // byte
+        // 8 bit signed integers (positive and negative)
+        byte byt = 8;
+
+        // short
+        // 16 bit signed integers
+        short shrt = 10;
+
+        // int
+        // 32 bit signed integers
+        int i = 199;
+
+        // long
+        // 64 bit signed integers
+        long l = 9988;
+
+        // float
+        // 32 bit signed floating values
+        float f = 10.5f;
+
+        // double
+        // 64 bit signed floating values
+        double d = 1667.88;
+
+        // chr
+        // 16 bit uncode characters
+        char c = 'a';
+        char b = '\u0000';
+
+        // boolean
+        boolean isTrue = false;
+
+        // print all the variable values
+        System.out.println(byt);
+        System.out.println(shrt);
+        System.out.println(i);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(c);
+        System.out.println(isTrue);
+
+    }
+
+}
