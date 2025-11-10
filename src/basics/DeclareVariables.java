b37d4f59a3488ed95af3089456907a3ce3bbe9fe
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2025-11-10 13:03:57 +0530

    add variables

---
+++ src/basics/DeclareVariables.java
@@ -0,0 +1,37 @@
+package basics;
+
+public class DeclareVariables {
+    public static void main(String[] args) {
+        // Variables are containers which hold information
+        // Variables should have name
+        // Type of the variable
+        // Syntax to declare a variable
+        // data_type variable_name;
+        // data_type variable_name = value;
+
+        // Numeric, Floating point values, Booleans, String, Date, Character
+
+        // Numeric type
+        int num; // variable without a value
+        int age = 25; // variable with value
+        System.out.println(age);
+
+        // Floating point
+        double price;
+        double cost = 10.99;
+        System.out.println(cost);
+
+        // character
+        char alpha = 'a';
+        char beta;
+        System.out.println(alpha);
+
+        // declare multiple variables
+        int x = 5, y = 10, z = 89;
+
+        // String
+        String name = "John";
+        System.out.println(name);
+
+    }
+}
