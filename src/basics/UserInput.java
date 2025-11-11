74509b89ef3200ad203cb1bf068ed2da75286add
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2025-11-11 09:48:00 +0530

    add user input

---
+++ src/basics/UserInput.java
@@ -0,0 +1,24 @@
+package basics;
+
+import java.util.Scanner;
+
+public class UserInput {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String name, city;
+        int age;
+        // scan.next();
+
+        System.out.println("Enter your city: ");
+        city = scan.nextLine(); // scan the text input from user
+
+        System.out.println("Enter your name: ");
+        name = scan.next(); // scan the single word input from user
+        System.out.println("Welcome " + name + " From " + city);
+
+        System.out.println("Enter your age: ");
+        age = scan.nextInt(); // scan the numeric input from user
+        System.out.println(" You are " + age + " years old");
+
+    }
+}
