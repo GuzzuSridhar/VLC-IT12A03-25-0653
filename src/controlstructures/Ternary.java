9be849a72162deeab977767d4f228bf5b30c514d
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2025-11-11 13:01:18 +0530

    add ter

---
+++ src/controlstructures/Ternary.java
@@ -0,0 +1,9 @@
+package controlstructures;
+
+public class Ternary {
+    public static void main(String[] args) {
+        int age = 18;
+        String res = age >= 18 ? "you can vote" : "you cannot vote";
+        System.out.println(res);
+    }
+}
