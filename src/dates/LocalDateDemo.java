63e2607c6e7562f76412c37f943c74e9c1020e67
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2025-11-13 08:54:57 +0530

    add dates

---
+++ src/dates/LocalDateDemo.java
@@ -0,0 +1,20 @@
+package dates;
+
+import java.time.LocalDate;
+
+public class LocalDateDemo {
+    public static void main(String[] args) {
+        LocalDate ld = LocalDate.now(); // current Date
+        System.out.println(ld);
+        System.out.println("---Access Date Fields---");
+        System.out.println(ld.getDayOfMonth());
+        System.out.println(ld.getMonth());
+        System.out.println(ld.getYear());
+        System.out.println(ld.getDayOfYear());
+
+        // Custom Date
+        LocalDate dob = LocalDate.of(1974, 3, 16);
+        System.out.println(dob);
+        System.out.println(dob.getMonth() + "-" + dob.getDayOfMonth() + "-" + dob.getYear());
+    }
+}
