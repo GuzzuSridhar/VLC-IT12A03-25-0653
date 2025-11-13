7fe5ab166bb8ea1b95d9340def8a970d3de74376
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2025-11-13 12:34:18 +0530

    add big

--- src/mathclass/Bigtypes.java
+++ src/mathclass/Bigtypes.java
@@ -1,4 +1,4 @@
-package basics;
+package mathclass;
 
 import java.math.BigDecimal;
 import java.math.BigInteger;
@@ -7,6 +7,7 @@ public class Bigtypes {
     public static void main(String[] args) {
         BigInteger bigInteger = new BigInteger("15554446666888");
         BigDecimal bigDecimal = new BigDecimal(0.1);
+        System.out.println(bigInteger);
         System.out.println(bigDecimal);
         BigDecimal bigDecimalOne = new BigDecimal("0.1");
         System.out.println(bigDecimalOne);
