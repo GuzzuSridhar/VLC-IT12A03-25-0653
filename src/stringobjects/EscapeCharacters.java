f3bddf84c73e071ba0c2f637530f127bfd311e31
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2025-11-13 14:14:13 +0530

    add color

---
+++ src/stringobjects/EscapeCharacters.java
@@ -0,0 +1,19 @@
+package stringobjects;
+
+public class EscapeCharacters {
+    public static void main(String[] args) {
+        /*
+         * \n -- new line
+         * \" -- double quotes
+         * \t -- tab
+         * \' -- apostrophe
+         * \\ -- back slash
+         */
+
+        System.out.println("This is Alex\'s Computer");
+        System.out.println("This is \"Alex\'s\" Computer");
+        System.out.println("This is in \n new line");
+        System.out.println("Name\tAge\tdepartment");
+        System.out.println("This is a backslash \\");
+    }
+}
--- src/stringobjects/TextBlock.java
+++ src/stringobjects/TextBlock.java
@@ -10,5 +10,6 @@ public class TextBlock {
                 """;
 
         System.out.println(pf);
+
     }
 }
