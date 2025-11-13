d99832ca89b615d59e5d66b1f40f09e6083f98f8
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2025-11-13 09:11:42 +0530

    add datetime

---
+++ src/dates/DateTimeDemo.java
@@ -0,0 +1,17 @@
+package dates;
+
+import java.time.LocalDateTime;
+
+public class DateTimeDemo {
+    public static void main(String[] args) {
+        LocalDateTime today = LocalDateTime.now();
+        System.out.println(today);
+
+        System.out.println(today.getDayOfMonth());
+        System.out.println(today.getMonth());
+        System.out.println(today.getYear());
+        System.out.println(today.getHour());
+        System.out.println(today.getMinute());
+        System.out.println(today.getSecond());
+    }
+}
